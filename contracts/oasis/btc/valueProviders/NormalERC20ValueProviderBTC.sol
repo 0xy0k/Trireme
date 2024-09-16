307cbc5ff1d1c862ccd5a79f72103d6a7133a1bf
0xy0k <dbkey.one1@gmail.com>
2024-09-16 12:49:59 +1000

    feat: add wbtc

---
+++ contracts/oasis/btc/valueProviders/NormalERC20ValueProviderBTC.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+import '../ERC20ValueProviderBTC.sol';
+
+contract NormalERC20ValueProviderBTC is ERC20ValueProviderBTC {
+    /// @notice This function is only called once during deployment of the proxy contract. It's not called after upgrades.
+    /// @param _aggregator The token oracles aggregator
+    /// @param _token The token address
+    /// @param _baseCreditLimitRate The base credit limit rate
+    /// @param _baseLiquidationLimitRate The base liquidation limit rate
+    function initialize(
+        IChainlinkV3Aggregator _aggregator,
+        IERC20MetadataUpgradeable _token,
+        RateLib.Rate calldata _baseCreditLimitRate,
+        RateLib.Rate calldata _baseLiquidationLimitRate
+    ) external initializer {
+        __initialize(
+            _aggregator,
+            _token,
+            _baseCreditLimitRate,
+            _baseLiquidationLimitRate
+        );
+    }
+}
